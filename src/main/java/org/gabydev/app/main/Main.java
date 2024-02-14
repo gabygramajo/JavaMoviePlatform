e63966bb75f88f6631bc555c8bbbb78365516aa3
Gaby <ggabrieldeveloper@gmail.com>
2024-02-14 18:29:00 -0300

    add: Implement methods

--- src/main/java/org/gabydev/app/main/Main.java
+++ src/main/java/org/gabydev/app/main/Main.java
@@ -1,25 +1,67 @@
 package org.gabydev.app.main;
 
-
+import org.gabydev.app.model.Genre;
+import org.gabydev.app.model.Movie;
 import org.gabydev.app.model.User;
 
 public class Main {
     public static void main(String[] args) {
 
-        User user = User.getUserById(2);
+        User user = User.getUserById(3);
 
+        // Listar catálogo
+        System.out.println("------ Catálogo de películas ------");
+        user.findAll()
+                .forEach(System.out::println);
+
+        // Encontrar película por id
+        System.out.println("\n------ Película por id ------");
+        Movie movie = user.findMovieById(16);
+        System.out.println("movie: " + movie);
+
+        // Listar películas por director
+        System.out.println("\n------ Película por director ------");
+        String director = movie.getDirector().getFullName();
+        user.findByDirector(director)
+                .forEach(System.out::println);
+
+        // Listar películas por género
+        System.out.println("\n------ Película por género ------");
+        user.findByGenre(Genre.ACTION.getGenre())
+                .forEach(System.out::println);
+
+        // Usuario a modificar
+        System.out.println("\n------ User a modificar ------");
         System.out.println(user);
 
-//        user.deleteFavoriteMovie(12);
+        // Modificar datos
+        user.updateFullName("Emily Lucy Davis");
+        user.updateNickname("LucyEmily");
+        user.updateEmail("emily_Lucy@mail.com");
+        user.updatePassword("P@ss123456");
 
-        user.deleteFavoriteMovie(13);
+        System.out.println("\n------ Datos Actualizados ------");
+        System.out.println(user);
 
-//        user.updateFullName("Emily Davis");
-//        user.updateNickname("Emily");
-//        user.updateEmail("emily_davise@mail.com");
-//        user.updatePassword("P@ss123456");
+        // Mostrar lista de películas favoritas
+        System.out.println("\n------ Lista de películas favoritas ------");
+        user.getMyFavoriteMovies()
+                .forEach(System.out::println);
 
-//        System.out.println("Movies");
-//        user.getMyFavoriteMovies().forEach(System.out::println);
+        // Agregar películas a la Lista de favoritos
+        user.addToFavoriteMovieList(11);
+        user.addToFavoriteMovieList(27);
+
+        System.out.println("\n------ Lista de películas favoritas Actualizada ------");
+        user.getMyFavoriteMovies()
+                .forEach(System.out::println);
+
+        // Eliminar película de la Lista de favoritos
+        user.deleteFavoriteMovie(27);
+
+        System.out.println("\n------ Lista de películas favoritas Actualizada ------");
+        user.getMyFavoriteMovies()
+                .forEach(System.out::println);
+
     }
 }
